e80ec4eb5032aa679716e3b72469040551e7bbe8
tony <zhujf620@gmail.com>
2017-09-14 14:59:21 +0800

    feat: 增加公用工具函数trim，用于清理爬取数据中的空格

---
+++ utils/trim.js
@@ -0,0 +1,35 @@
+/**
+ * @author tongc726 <zhujf620@gmail.com>
+ */
+
+// 所有可能的空白字符
+const whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+
+/**
+ * 去除字符串中的空白字符
+ * @see http://www.cnblogs.com/rubylouvre/archive/2009/09/18/1568794.html
+ *
+ * @param {String} str - 待处理的字符串
+ * @return {String}
+ */
+export default (str = '') => {
+  if (!str || str.length === 0) {
+    return str;
+  }
+  let result = str;
+  for (let i = 0, len = result.length; i < len; i += 1) {
+    if (whitespace.indexOf(result.charAt(i)) === -1) {
+      result = result.substring(i);
+      break;
+    }
+  }
+
+  for (let j = result.length - 1; j >= 0; j -= 1) {
+    if (whitespace.indexOf(result.charAt(j)) === -1) {
+      result = result.substring(0, j + 1);
+      break;
+    }
+  }
+
+  return result;
+};
